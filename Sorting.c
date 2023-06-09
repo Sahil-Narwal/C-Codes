6f8755d59d52244aed246c23cadd375bd9cf9c1f
Sahil Narwal <globellyunique1@gmail.com>
2023-06-09 13:54:33 +0530

    Improved to perform to different task

--- Sorting.c
+++ Sorting.c
@@ -1,37 +1,79 @@
 #include <stdio.h>
-// To arrange number in accending
+// To arrange numbers in accending or decending order
 int main()
 {
-    int i, j, n, temp;
+    int i, j, n, temp, choice;
     printf("Enter the size of Array:\n");
     scanf("%d", &n);
+    n = n--;
     int a[n];
-    printf("Enter the numbers :\n");
-    for (i = 0; i < n; i++)
+    printf("In which order you want the series:\n");
+    printf("1 Accending\n");
+    printf("2 Decending\n");
+    scanf("%d", &choice);
+    switch (choice)
     {
-        scanf("%d", &a[i]);
-    }
-    printf("The Entered numbers are ");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\t", a[i]);
-    }
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
+    case 1:
+        printf("Enter the numbers :\n");
+        for (i = 0; i < n; i++)
         {
-            if (a[i] > a[j])
+            scanf("%d", &a[i]);
+        }
+        printf("The Entered numbers are ");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+        for (i = 0; i < n; i++)
+        {
+            for (j = i + 1; j < n; j++)
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
-    }
-    printf("The sorted array is\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\t", a[i]);
+        printf("The sorted array is\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+        break;
+    case 2:
+        printf("Enter the numbers :\n");
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        printf("The Entered numbers are ");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+        for (i = 0; i < n; i++)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                if (a[i] <= a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        printf("The sorted array is\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+        break;
+    default:
+        printf("Invalid input");
+        break;
     }
     return 0;
 }
