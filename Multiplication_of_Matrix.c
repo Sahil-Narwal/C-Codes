bbcb68383a3819beca5acd34d14281a4e9c9278a
Sahil Narwal <globellyunique1@gmail.com>
2023-06-13 13:50:43 +0530

    Updated for multitasking

--- Multiplication_of_Matrix.c
+++ Multiplication_of_Matrix.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 int main()
 {
-    int a[3][3], b[3][3], C[3][3], r, c, k;
-    printf("Enter elements of first 3*3 matrix: \n");
-    for (r = 0; r < 3; r++)
-        for (c = 0; c < 3; c++)
+    int nor1, noc1, nor2, noc2;
+    printf("Enter the number of rows and columns for the first matrix\n");
+    scanf("%d%d", &nor1, &noc1);
+    printf("Enter the number of rows and columns for the second matrix\n");
+    scanf("%d%d", &nor2, &noc2);
+    
+    if (noc1 != nor2)
+    {
+        printf("Error: The number of columns in the first matrix must be equal to the number of rows in the second matrix\n");
+        return 0;
+    }
+    
+    int a[nor1][noc1], b[nor2][noc2], C[nor1][noc2], r, c, k;
+    printf("Enter elements of first matrix: \n");
+    for (r = 0; r < nor1; r++)
+        for (c = 0; c < noc1; c++)
         {
             scanf("%d", &a[r][c]);
         }
-    printf("Enter elements of first 3*3 matrix: \n");
-    for (r = 0; r < 3; r++)
-        for (c = 0; c < 3; c++)
+    printf("Enter elements of second matrix: \n");
+    for (r = 0; r < nor2; r++)
+        for (c = 0; c < noc2; c++)
         {
             scanf("%d", &b[r][c]);
         }
-    printf("\nThe multiplication of two matrics : \n");
-    for (r = 0; r < 3; r++)
+    printf("\nThe multiplication of two matrices : \n");
+    for (r = 0; r < nor1; r++)
     {
-        for (c = 0; c < 3; c++)
+        for (c = 0; c < noc2; c++)
         {
-            for (k = 0; k < 3; k++)
+            C[r][c] = 0;
+            for (k = 0; k < noc1; k++)
             {
-                C[r][c] = 0;
                 C[r][c] = C[r][c] + (a[r][k] * b[k][c]);
             }
             printf("%d\t", C[r][c]);
