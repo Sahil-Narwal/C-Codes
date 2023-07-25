c4118ba51cd4655caf88c49b0571ec62f9b4cdfd
Sahil-Narwal <globellyunique1@gmail.com>
2023-07-25 21:53:16 +0530

    Dynamic Memory Allocation programmes

---
+++ DynamicM_Free.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *ptr;
+    int i, n, m;
+    printf("Enter the size of memory you want to allocate:\n");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("The entered four numbers are:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+    printf("Enter the size of memory you want to allocate:\n");
+    scanf("%d", &m);
+    free(ptr);
+    ptr = (int *)calloc(m, sizeof(int));
+    printf("Enter %d numbers:\n", m);
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("The entered four numbers are:\n");
+    for (i = 0; i < m; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+    return 0;
+}
---
+++ DynamicM_Malloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Dynamic memory allocation (malloc)
+int main()
+{
+    int *ptr;
+    int i, n;
+    printf("Enter the size of memory you want to allocate:\n");
+    scanf("%d", &n);
+    ptr = (int *)malloc(n * sizeof(int));
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+    printf("The entered numbers are:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+    return 0;
+}
